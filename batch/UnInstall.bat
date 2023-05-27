25893129a78b4134059c4064acb7a30022dd3d43
yangnuozhen <56541171+yangnuozhen@users.noreply.github.com>
2023-05-27 20:05:19 +0800

    refactor : Replace batch files

---
+++ batch/UnInstall.bat
@@ -0,0 +1,42 @@
+@ECHO off
+CLS
+:init
+setlocal DisableDelayedExpansion
+set "batchPath=%~0"
+for %%k in (%0) do set batchName=%%~nk
+set "vbsGetPrivileges=%temp%\OEgetPriv_%batchName%.vbs"
+setlocal EnableDelayedExpansion
+:checkPrivileges
+NET FILE 1>NUL 2>NUL
+if '%errorlevel%' == '0' ( goto gotPrivileges ) else ( goto getPrivileges )
+:getPrivileges
+if '%1'=='ELEV' (ECHO ELEV & shift /1 & goto gotPrivileges)
+ECHO.
+ECHO ********************************
+ECHO 请求 UAC 权限批准……
+ECHO ********************************
+ECHO Set UAC = CreateObject^("Shell.Application"^) > "%vbsGetPrivileges%"
+ECHO args = "ELEV " >> "%vbsGetPrivileges%"
+ECHO For Each strArg in WScript.Arguments >> "%vbsGetPrivileges%"
+ECHO args = args ^& strArg ^& " "  >> "%vbsGetPrivileges%"
+ECHO Next >> "%vbsGetPrivileges%"
+ECHO UAC.ShellExecute "!batchPath!", args, "", "runas", 1 >> "%vbsGetPrivileges%"
+"%SystemRoot%\System32\WScript.exe" "%vbsGetPrivileges%" %*
+exit /B
+:gotPrivileges
+setlocal & pushd .
+cd /d %~dp0
+if '%1'=='ELEV' (del "%vbsGetPrivileges%" 1>nul 2>nul  &  shift /1)
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+::     安装过程     ::
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:-------------------------------------- 
+set "starttime=%time%"
+net stop ZFile
+ZFile.exe -u
+ECHO.
+set "endtime=%time%"
+set /a "times=(%endtime:~,2%-%starttime:~,2%%add%)*360000+(1%endtime:~3,2%%%100-1%starttime:~3,2%%%100)*6000+(1%endtime:~6,2%%%100-1%starttime:~6,2%%%100)*100+(1%endtime:~-2%%%100-1%starttime:~-2%%%100)" 
+ECHO  卸载完成.................................%times%ms【OK】  
+pause 
+exit
